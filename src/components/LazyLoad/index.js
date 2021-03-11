9bdc45472645d6e9d41d375904eecf036184c43f
minhhieu1612 <hieunguyenminh562@gmail.com>
2021-03-11 17:56:42 +0700

    remove comment

--- src/components/LazyLoad/index.js
+++ src/components/LazyLoad/index.js
@@ -8,45 +8,6 @@ import {
 import loadingGif from "./loading.gif";
 import "./index.css";
 
-// const LazyLoad = ({ src }) => {
-//   const rootRef = useRef();
-//   const [isVisible, setIsVisible] = useState(false);
-//   const loading =
-//     "https://tom.imgix.net/imgix_logo_curvesq.png?auto=compress&q=1&blur=100";
-
-//   useEffect(() => {
-//     const defaultIntersectionOptions = {
-//       threshold: 0,
-//       rootMargin: "0px",
-//     };
-
-//     const checkIntersections = (entries) => {
-//       entries.forEach((entry) => {
-//         if (entry.isIntersecting) {
-//           setIsVisible(true);
-//         }
-//       });
-//     };
-
-//     if (!isVisible) {
-//       const newIO = new IntersectionObserver(
-//         checkIntersections,
-//         defaultIntersectionOptions
-//       );
-//       newIO.observe(rootRef.current);
-//       return () => newIO.disconnect();
-//     }
-//   }, [isVisible]);
-
-//   return (
-//     <img
-//       height="600px"
-//       src={isVisible ? src : loading}
-//       ref={rootRef}
-//       sizes="(min-width: 1240px) 30vw, 90vw"
-//     />
-//   );
-// };
 class LazyLoad extends React.Component {
   constructor(props) {
     super(props);
